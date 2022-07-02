79208468d4166aca52d074a58201711a63aea1bd
Sarmad Mujtaba <srmdmjtba@gmail.com>
2022-07-02 10:41:13 +0000

    Correcting Mysql

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -16,52 +18,78 @@ type Users struct {
 	Pass string `json:"pass"`
 }
 
+var conn *sql.DB
+
 func main() {
-	fmt.Println("System Running ...")
+	const (
+		host     = "localhost"
+		port     = 3306
+		user     = "root"
+		password = "DummySQL786"
+		dbname   = "users"
+	)
+	connString := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s", user, password, host, port, dbname)
+
+	var err error
+	conn, err = sql.Open("mysql", connString)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection Established...")
 	API()
 }
 
 func getData(w http.ResponseWriter, r *http.Request) {
-	var user []Users
-	fileData, err := ioutil.ReadFile("users.json")
-	json.Unmarshal(fileData, &user)
+	var users []Users
+	var user Users
+
+	insert, err := conn.Query("select * from user_data")
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Failed to Fetch!!")
+	}
+	for insert.Next() {
+		insert.Scan(&user.ID, &user.Name, &user.Pass)
+		users = append(users, user)
+	}
 	var count int = 0
 	id := r.URL.Query().Get("id")
 	if len(id) > 0 {
-		for _, v := range user {
+		for _, v := range users {
 			if v.ID == id {
 				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(user[count])
+				json.NewEncoder(w).Encode(users[count])
 				return
 			}
 			count++
 		}
 	}
-	if err != nil {
-		fmt.Fprintf(w, "File not found!!")
-		w.WriteHeader(404)
-		return
-	}
-	json.Unmarshal(fileData, &user)
+	json.Marshal(users)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(users)
+	return
 }
 
 func postData(w http.ResponseWriter, r *http.Request) {
-
-	var user []Users
+	var users []Users
+	var user Users
 	var add Users
-	fileData, err := ioutil.ReadFile("users.json")
+
+	get, err := conn.Query("select * from user_data")
 	if err != nil {
-		fmt.Fprintf(w, "File not found!!")
-		w.WriteHeader(404)
-		return
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Failed to Insert!!")
 	}
-	json.Unmarshal(fileData, &user)
+	for get.Next() {
+		get.Scan(&user.ID, &user.Name, &user.Pass)
+		users = append(users, user)
+	}
 	dataFromWeb, _ := ioutil.ReadAll(r.Body)
 	var dataToCompare map[string]string
 	json.Unmarshal(dataFromWeb, &dataToCompare)
-	for _, v := range user {
+	for _, v := range users {
 		if v.ID == dataToCompare["id"] {
 			w.WriteHeader(409)
 			fmt.Fprintf(w, "ID already in use!!")
@@ -71,17 +99,16 @@ func postData(w http.ResponseWriter, r *http.Request) {
 	add.ID = dataToCompare["id"]
 	add.Name = dataToCompare["name"]
 	add.Pass = dataToCompare["pass"]
-	user = append(user, add)
-	out, _ := json.MarshalIndent(user, "", "\t")
-	_ = ioutil.WriteFile("users.json", out, 0755)
+	conn.Query("insert into user_data values(?, ?, ?)", add.ID, add.Name, add.Pass)
 	w.WriteHeader(201)
-	fmt.Fprintf(w, "User Created!!")
+	fmt.Fprintf(w, "User inserted!!")
 	return
+
 }
 
 func API() {
 	r := mux.NewRouter()
 	r.HandleFunc("/users", getData).Methods("GET")
 	r.HandleFunc("/users", postData).Methods("POST")
-	log.Fatal(http.ListenAndServe(":5005", r))
+	log.Fatal(http.ListenAndServe(":5010", r))
 }
