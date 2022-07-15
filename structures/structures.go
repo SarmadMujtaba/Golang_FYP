422129037ced63d9ecb6fe356d5f5e9c18fa96d2
Sarmad Mujtaba <srmdmjtba@gmail.com>
2022-07-15 15:57:53 +0500

    Including CreatedAT field

--- structures/structures.go
+++ structures/structures.go
@@ -1,24 +1,29 @@
 package structures
 
+import "time"
+
 type Users struct {
-	ID    string `json:"id" validate:"uuid" gorm:"primaryKey;autoIncrement:false"`
-	Name  string `json:"name" validate:"alpha"`
-	Email string `json:"email" validate:"email"`
-	Pass  string `json:"pass" validate:"alphanum"`
+	ID        string `json:"id" validate:"uuid" gorm:"primaryKey;autoIncrement:false"`
+	Name      string `json:"name" validate:"alpha"`
+	Email     string `json:"email" validate:"email"`
+	Pass      string `json:"pass" validate:"alphanum"`
+	CreatedAt time.Time
 }
 
 type Organizations struct {
-	Org_ID  string `json:"id" validate:"uuid" gorm:"primaryKey;autoIncrement:false"`
-	Name    string `json:"name"`
-	About   string `json:"about"`
-	Website string `json:"website"`
-	U_ID    string `json:"user_id" validate:"uuid"`
+	Org_ID    string `json:"id" validate:"uuid" gorm:"primaryKey;autoIncrement:false"`
+	Name      string `json:"name"`
+	About     string `json:"about"`
+	Website   string `json:"website"`
+	U_ID      string `json:"user_id" validate:"uuid"`
+	CreatedAt time.Time
 }
 
 type Memberships struct {
-	ID     string `json:"pk" gorm:"primaryKey;autoIncrement:false"`
-	U_ID   string `json:"id" validate:"uuid"`
-	Org_ID string `json:"org_id" validate:"uuid"`
+	ID        string `json:"pk" gorm:"primaryKey;autoIncrement:false"`
+	U_ID      string `json:"id" validate:"uuid"`
+	Org_ID    string `json:"org_id" validate:"uuid"`
+	CreatedAt time.Time
 	// Users         Users         `gorm:"foreignKey:U_ID;references:ID"`
 	// Organizations Organizations `gorm:"foreignKey:Org_ID;references:Org_ID"`
 }
