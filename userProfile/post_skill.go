e72843774ed0853f434f0a28003b4520e88e8413
Sarmad191387 <191387@students.au.edu.pk>
2023-04-30 12:37:03 +0500

    adding api to send skills to python_db

--- userProfile/post_skill.go
+++ userProfile/post_skill.go
@@ -3,6 +3,7 @@ package userprofile
 import (
 	"PostJson/db"
 	"PostJson/structures"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -25,6 +26,8 @@ import (
 func AddSkill(w http.ResponseWriter, r *http.Request) {
 	var skill structures.Skills
 	var user []structures.Users
+	var skills []structures.Skills
+	var Skills []string
 
 	dataFromWeb, _ := ioutil.ReadAll(r.Body)
 	var dataToCompare map[string]string
@@ -70,6 +73,46 @@ func AddSkill(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Could not add skill!!")
 		return
 	}
+
+	// sending skills to python_db
+	result2 := db.Conn.Model(&skills).Select("skill").Find(&skills)
+	if result2.Error != nil {
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "Nothing to return")
+		return
+	}
+
+	// creating json array of skills
+	for _, v := range skills {
+		Skills = append(Skills, v.Skill)
+	}
+
+	data, _ := json.Marshal(Skills)
+
+	// change url with python's url later. It is Path parameter after url
+	posturl := "http://host.docker.internal:8000/skills/" + skill.U_ID
+
+	// concurently sending request to python
+	go SendRequest(posturl, data)
+
 	w.WriteHeader(201)
 	fmt.Fprintf(w, "Skill added!!")
+}
+
+func SendRequest(url string, data []byte) {
+	r, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	r.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(resp.StatusCode)
 }
